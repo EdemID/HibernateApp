30c9ec2c32ff9f987c4a8ba19193064d57fce214
EdemID <00350035aA>
2023-01-11 11:37:50 +0500

    Many-to-many

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,14 @@
 package org.example;
 
-import org.example.model.Passport;
-import org.example.model.Person;
+import org.example.model.Actor;
+import org.example.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,8 +17,8 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -34,26 +37,56 @@ public class App {
     }
 
     private static void save(Session session) {
-        Person person = new Person("Ivan", 32);
-        Passport passport = new Passport(12345);
-        person.setPassport(passport);
+        Movie movie = new Movie("Film", 1994);
+        Actor actor1 = new Actor("1", 23);
+        Actor actor2 = new Actor("2", 34);
 
-        session.save(person);
+        movie.setActors(new ArrayList<>(Arrays.asList(actor1, actor2)));
+        actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//        actor2.setMovies(new ArrayList<>(List.of(movie)));
+
+        session.save(movie);
+        session.save(actor1);
+        session.save(actor2);
     }
 
-    private static void getPerson(Session session) {
-        Passport passport = session.get(Passport.class, 1);
-        Person person = passport.getPerson();
+    private static void getActors(Session session) {
+        Movie movie = session.get(Movie.class, 1);
+        System.out.println(movie.getActors());
     }
 
-    private static void updatePassport(Session session) {
-        Person person = session.get(Person.class, 1);
-        Passport passport = person.getPassport();
-        passport.setPassportNumber(678789);
+    private static void getMovies(Session session) {
+        Actor actor = session.get(Actor.class, 2);
+        System.out.println(actor.getMovies());
     }
 
+    private static void newFilm(Session session) {
+        Movie movie = new Movie("Film2", 1998);
+        Actor actor = session.get(Actor.class, 1);
+
+//        movie.setActors(List.of(actor));
+        movie.setActors(Arrays.asList(actor));
+        actor.getMovies().add(movie);
+
+        session.save(movie);
+    }
+//
+//    private static void updatePassport(Session session) {
+//        Person person = session.get(Person.class, 1);
+//        Passport passport = person.getPassport();
+//        passport.setPassportNumber(678789);
+//    }
+//
     private static void delete(Session session) {
-        Person person = session.get(Person.class, 1);
-        session.remove(person);
+        Actor actor = session.get(Actor.class, 2);
+        System.out.println(actor);
+
+        int movieIndex = 0;
+
+        Movie movie = actor.getMovies().get(movieIndex);
+        System.out.println(movie);
+
+        actor.getMovies().remove(movieIndex);
+        movie.getActors().remove(actor);
     }
 }
